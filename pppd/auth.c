e28022bce0643c3b9f455d8903a3af3ca4467314
dongchen0502 <857940431@qq.com>
2020-04-22 14:08:10 +0800

    Update auth.c
    debug

--- pppd/auth.c
+++ pppd/auth.c
@@ -774,7 +774,7 @@ warn("2got neg_upap = %d", go->neg_upap);
 	if (noauth_addrs != NULL) {
 	    set_allowed_addrs(unit, NULL, NULL);
 	} else if (!wo->neg_upap || uselogin || !null_login(unit)) {
-		go->neg_upap = 1;
+		go->neg_chap = 1;
 		warn("got neg_upap = %d", go->neg_upap);
 		warn("got neg_chap = %d", go->neg_chap);
 		warn("got neg_eap = %d", go->neg_eap);
